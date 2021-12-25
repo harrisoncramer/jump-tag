eae40a7fca037057dcba9289a62a689733b5fb93
Harrison Cramer <harrisoncramer@gmail.com>
2021-12-25 17:44:17 -0500

    Added verbosity.

--- lua/jump-tag/init.lua
+++ lua/jump-tag/init.lua
@@ -1,10 +1,17 @@
 local ts_utils = require("nvim-treesitter.ts_utils")
 
+local opts = {
+  verbose = false
+}
+
 local get_tag_name_from_element
 get_tag_name_from_element = function (node)
   for n in node:iter_children() do
     if(n == nil) then
-      error("That node has no children")
+      if opts.verbose == true then
+        error("That node has no children")
+      end
+      return nil
     end
     local type = n:type()
     if type == 'tag_name' then
@@ -22,13 +29,19 @@ local get_prev_sibling_of_same_type = function (node)
   local type = node:type()
   local sibling = node:prev_sibling()
   if(sibling == nil) then
-    error("There is no previous sibling")
+      if opts.verbose == true then
+        error("There is no previous sibling")
+      end
+      return nil
   end
 
   while sibling:type() ~= type do
     sibling = sibling:prev_sibling()
     if(sibling == nil) then
-      error("There is no previous sibling")
+      if opts.verbose == true then
+        error("There is no previous sibling")
+      end
+      return nil
     end
   end
   return sibling
@@ -38,13 +51,19 @@ local get_next_sibling_of_same_type = function (node)
   local type = node:type()
   local sibling = node:next_sibling()
   if(sibling == nil) then
-    error("There is no next siling")
+    if opts.verbose == true then
+      error("There is no next siling")
+    end
+    return nil
   end
 
   while sibling:type() ~= type do
     sibling = sibling:next_sibling()
     if(sibling == nil) then
-      error("There is no next sibling")
+      if opts.verbose == true then
+        error("There is no next sibling")
+      end
+      return nil
     end
   end
   return sibling
@@ -53,11 +72,17 @@ end
 local get_parent = function (node)
   local prev = ts_utils.get_previous_node(node, true, true)
   if(prev == nil) then
-    error("This node has no parent")
+    if opts.verbose == true then
+      error("There is no parent")
+    end
+    return nil
   end
   while(prev:parent() == node:parent()) do
     if(prev == nil) then
-      error("This node has no parent")
+      if opts.verbose == true then
+        error("There is no parent")
+      end
+      return nil
     end
     node = prev
     if(ts_utils.get_previous_node(prev, true, true) == nil) then
@@ -86,13 +111,22 @@ end
 local jumpParent = function ()
   local node = get_master_node()
   local parent = get_parent(node)
+  if(parent == nil) then
+    return
+  end
   local tag_name = parent:child(1)
+  if(tag_name == nil) then
+    return
+  end
   ts_utils.goto_node(tag_name)
 end
 
 local jumpNextSibling = function ()
   local node = get_master_node()
   local sibling = get_next_sibling_of_same_type(node)
+  if(sibling == nil) then
+    return
+  end
   local tag_name = get_tag_name_from_element(sibling)
   ts_utils.goto_node(tag_name)
 end
@@ -100,6 +134,9 @@ end
 local jumpPrevSibling = function ()
   local node = get_master_node()
   local sibling = get_prev_sibling_of_same_type(node)
+  if(sibling == nil) then
+    return
+  end
   local tag_name = get_tag_name_from_element(sibling)
   ts_utils.goto_node(tag_name)
 end
